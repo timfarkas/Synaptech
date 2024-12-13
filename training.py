d7071117591d5fc92fa0ac7cfccc1edc26c77fdb
Tim Farkas <mail@timfarkas.com>
2024-12-13 11:20:31 +0000

    added .env support for pointing to dataset

--- training.py
+++ training.py
@@ -1,10 +1,16 @@
 from dataset.dataSet import OpenFMRIDataSet
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
+dataset_path = os.getenv("DATASET_PATH")
+
 
 ### init dataset this will automatically download and preprocess the dataset if run for the first time
-train_dataset = OpenFMRIDataSet(mode='train')
-val_dataset = OpenFMRIDataSet(mode='val',loadData=False)
-test_dataset = OpenFMRIDataSet(mode='test',loadData=False) 
-
-
+train_dataset = OpenFMRIDataSet(mode='train',datasetPath=dataset_path)
+val_dataset = OpenFMRIDataSet(mode='val',datasetPath=dataset_path,loadData=False)
+test_dataset = OpenFMRIDataSet(mode='test',datasetPath=dataset_path,loadData=False) 
 
 
